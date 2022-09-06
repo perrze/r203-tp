123ca51d482cd9067d5ff45273be6d717b6cdb8d
sheep0lata <112927338+sheep0lata@users.noreply.github.com>
2022-09-06 12:01:20 +0200

    Update scriptDHCP.sh
    commentaires et petites retouches

--- scriptDHCP.sh
+++ scriptDHCP.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
 
-sudo apt update
+sudo apt update                                                     #mise à jour du paquet apt
 sudo apt upgrade
 
-if [ -d "/etc/dhcp" ]
-then 
+if [ -d "/etc/dhcp" ]                                               #test pour savoir si le paquet isc-dhcp-server est installé
+then                                                                #si oui, on supprime le paquet pour le réinstaller
 apt-get remove --purge udhcpd
 sudo apt-get install isc-dhcp-server
-else
+else                                                                #sinon on installe le paquet
 sudo apt-get install isc-dhcp-server
 fi
 
 
-mv /etc/dhcp/dhcpd.conf /etc/dhcp/cpDhcpd.conf
+mv /etc/dhcp/dhcpd.conf /etc/dhcp/cpDhcpd.conf                      #on renomme le fichier de configuration initial et on en recréé un nouveau
 touch /etc/dhcp/dhcpd.conf
+                                                                                          #on demande à l'utilisateur toutes les informmations à avoir dans notre DHCP:
+read -p "entrez le numéro de réseau distribué par le DHCP : " netNumber                   #numéro de réseau
+read -p "entrez le masque de sous-réseau distribué par le DHCP : " netMask                #masque de sous-réseau
+read -p "entrez la première adresse distribuée par le DHCP : " firstAddr                  #première adresse
+read -p "entrez le dernière adresse distribuée par le DHCP : " lastAddr                   #dernière adresse
+read -p "entrez le routeur par défaut distribué par le DHCP : " router                    #routeur
+read -p "entrez l'adresse de broadcast distribuée par le DHCP : " broadcast               #adresse de broadcast du réseau
+read -p "entrez le serveur DNS distribué par le DHCP : " dns                              #adresse du dns 
+read -p "entrez la durée de bail par défaut : " defLease                                  #durée de bail par défaut
+read -p "entrez la durée de bail maximale : " maxLease                                    #durée maximale de bail
 
-ip addr add $ipServ"/"$netMask dev int
-ead -p echo "rentrez le nom de l'interface :" int
-
-read -p "entrez le numéro de réseau distribué par le DHCP : " netNumber
-read -p "entrez le masque de sous-réseau distribué par le DHCP : " netMask
-read -p "entrez la première adresse distribuée par le DHCP : " firstAddr
-read -p "entrez le dernière adresse distribuée par le DHCP : " lastAddr
-read -p "entrez le routeur par défaut distribué par le DHCP : " router
-read -p "entrez l'adresse de broadcast distribuée par le DHCP : " broadcast
-read -p "entrez le serveur DNS distribué par le DHCP : " dns
-read -p "entrez la durée de bail par défaut : " defLease
-read -p "entrez la durée de bail maximale : " maxLease
-
-echo "subnet $netNumber netmask $netMask {" >> /etc/dhcp/dhcpd.conf
+echo "subnet $netNumber netmask $netMask {" >> /etc/dhcp/dhcpd.conf                       #on ajoute toutes les informations dans le fichier de configuration de notre serveur
 echo "range $firstAddr $lastAddr ;" >> /etc/dhcp/dhcpd.conf
 echo "option routers $router ;" >> /etc/dhcp/dhcpd.conf
 echo "option broadcast-address $broadcast ;" >> /etc/dhcp/dhcpd.conf
@@ -38,7 +35,7 @@ echo "default-lease-time $defLease ;" >> /etc/dhcp/dhcpd.conf
 echo "max-lease-time $maxLease ;" >> /etc/dhcp/dhcpd.conf
 echo "}" >> /etc/dhcp/dhcpd.conf
 
+read -p "rentrez le nom de l'interface :" int                                        #on demande l'interface sur laquelle fonctionnera le serveur
+echo "INTERFACESv4 = $int" >> /etc/default/isc-dhcp-server                           #on ajoute cette interface dans le fichier /etc/default/isc-dhcp-server
 
-echo "INTERFACESv4 = $int" >> /etc/default/isc-dhcp-server
-
-sudo service isc-dhcp-server restart
+sudo service isc-dhcp-server restart                                                 #on redémare le service pour appliquer les changements
